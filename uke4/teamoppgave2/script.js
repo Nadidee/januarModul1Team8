a0436c4741dc3491d9965c54830ba431756067e9
Nadidee <116063902+Nadidee@users.noreply.github.com>
2023-01-30 23:25:15 +0100

    uppload

--- uke4/teamoppgave2/script.js
+++ uke4/teamoppgave2/script.js
@@ -1,37 +1,37 @@
-const hungerBarDiv = document.querySelector('#hungerBar');
-const happinessBarDiv = document.querySelector('#happinessBar');
+// const hungerBarDiv = document.querySelector('#hungryBar');
+// const happinessBarDiv = document.querySelector('#happinessBar');
 
-let hunger = 100;
-let happiness = 100;
+// let hunger = 100;
+// let happiness = 100;
 
-function feed() {
-    hunger += 10;
-    if (hunger > 100) {
-        hunger = 100;
-    }
-    hungerBarDiv.innerText = hunger;
-}
-function play() {
-    happiness += 10;
-    if (happiness > 100) {
-        happiness = 100;
-    }
-    happinessBarDiv.innerText = happiness;
-}
+// function feed() {
+//     hunger += 10;
+//     if (hunger > 100) {
+//         hunger = 100;
+//     }
+//     hungerBarDiv.innerText = hunger;
+// }
+// function play() {
+//     happiness += 10;
+//     if (happiness > 100) {
+//         happiness = 100;
+//     }
+//     happinessBarDiv.innerText = happiness;
+// }
 
-setInterval(function() {
-    hunger -= 1;
-    happiness -= 1;
+// setInterval(function() {
+//     hunger -= 1;
+//     happiness -= 1;
 
-    hungerBarDiv.innerText = hunger;
-    happinessBarDiv.innerText = happiness;
+//     hungerBarDiv.innerText = hunger;
+//     happinessBarDiv.innerText = happiness;
 
-    if (hunger === 0 || happiness === 0) {
-        alert('Is dead :/');
-        hunger = 100;
-        happiness = 100;
-    }
-}, 1000);
+//     if (hunger === 0 || happiness === 0) {
+//         alert('Is dead :/');
+//         hunger = 100;
+//         happiness = 100;
+//     }
+// }, 1000);
 
 // let hunger = 100;
 // let happiness = 100;
@@ -40,3 +40,5 @@ setInterval(function() {
 //     hunger -= 1;
 //     happiness -= 1;
 // })
+
+
